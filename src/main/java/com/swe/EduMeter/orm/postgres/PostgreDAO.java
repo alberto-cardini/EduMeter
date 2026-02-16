7f7d21f21dd24b057cd011b73f3bce0890632875
carolina <carolinacecchi06@gmail.com>
2026-02-16 23:34:23 +0100

    [Feat] Add abstract PostgreDAO for generic query execution

---
+++ src/main/java/com/swe/EduMeter/orm/postgres/PostgreDAO.java
@@ -0,0 +1,66 @@
+package com.swe.EduMeter.orm.postgres;
+
+import com.swe.EduMeter.orm.DatabaseManager;
+
+import java.sql.Connection;
+import java.sql.PreparedStatement;
+import java.sql.ResultSet;
+import java.sql.SQLException;
+import java.util.ArrayList;
+import java.util.List;
+
+//Abstract class that handles executing queries and mapping results.
+
+public abstract class PostgreDAO<T> {
+    private void setParams(PreparedStatement stmt, List<Object> params) throws  SQLException {
+        for (int i = 0; i < params.size(); i++) {
+            stmt.setObject(i + 1, params.get(i));
+        }
+    }
+
+    // Converts every row of the ResultSet into a concrete object.
+    protected abstract T mapRowToObject(ResultSet rs) throws SQLException;
+
+
+    // Executes a SELECT query and returns a list of mapped objects.
+    //
+    // Accepting a query String is safe here because this method is protected.
+    // It is intended to be called only by concrete DAOs with internally defined queries,
+    // never with direct user input.
+    //
+    // The connection is not closed as it's managed by the DatabaseManager.
+    protected List<T> runQuery(String query, List<Object> params) throws SQLException {
+        Connection conn = DatabaseManager.getInstance().getConnection();
+
+        PreparedStatement stmt = conn.prepareStatement(query);
+        setParams(stmt, params);
+        stmt.closeOnCompletion();
+
+        try (ResultSet rs = stmt.executeQuery()) {
+            List<T> results = new ArrayList<>();
+            while (rs.next()) {
+                results.add(mapRowToObject(rs));
+            }
+            return results;
+        }
+    }
+
+    protected List<T> runQuery(String query) throws SQLException {
+        return  runQuery(query, List.of());
+    }
+
+    //Executes INSERT, DELETE, UPDATE queries.
+    protected void runUpdate(String query, List<Object> params) throws SQLException {
+        Connection conn = DatabaseManager.getInstance().getConnection();
+
+        try (PreparedStatement stmt = conn.prepareStatement(query)) {
+            setParams(stmt, params);
+
+            stmt.executeUpdate();
+        }
+    }
+
+    protected void runUpdate(String query) throws SQLException {
+        runUpdate(query, List.of());
+    }
+}
