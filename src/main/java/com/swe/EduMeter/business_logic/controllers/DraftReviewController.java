c2932408f1cf45d66d521b5ee5832f329a004a3f
TheRealLorenz <lollobellina@gmail.com>
2026-02-24 00:54:42 +0100

    [Feat] Add DraftReviewController

--- src/main/java/com/swe/EduMeter/business_logic/controllers/DraftReviewController.java
+++ src/main/java/com/swe/EduMeter/business_logic/controllers/DraftReviewController.java
@@ -1,36 +1,64 @@
 package com.swe.EduMeter.business_logic.controllers;
 
-import jakarta.ws.rs.Path;
+import com.swe.EduMeter.business_logic.auth.annotations.AdminGuard;
+import com.swe.EduMeter.business_logic.auth.annotations.AuthGuard;
+import com.swe.EduMeter.model.DraftReview;
+import com.swe.EduMeter.model.response.ApiObjectCreated;
+import com.swe.EduMeter.model.response.ApiOk;
+import com.swe.EduMeter.orm.DraftReviewDAO;
+import jakarta.inject.Inject;
+import jakarta.ws.rs.*;
+import jakarta.ws.rs.core.Context;
+import jakarta.ws.rs.core.MediaType;
+import jakarta.ws.rs.core.SecurityContext;
 
-//@Path("/review/draft")
+import java.time.LocalDate;
+import java.util.List;
+
+@Path("/review/draft")
 public class DraftReviewController {
-    // TODO
-    //@GET
-    //@Path("/pending")
-    ////@AdminGuard
-    //@Produces(MediaType.APPLICATION_JSON)
-    //public List<Review> getPending() {
-    //    return reviewDAO.list(ReviewStatus.PENDING);
-    //}
+    private final DraftReviewDAO reviewDAO;
 
-    //@PUT
-    //@Path("/{review_id}/approve")
-    ////@AdminGuard
-    //@Consumes(MediaType.APPLICATION_JSON)
-    //@Produces(MediaType.APPLICATION_JSON)
-    //public Boolean approve(@PathParam("review_id") int id, Review validatedReview)
-    //{
-    //    validatedReview.setId(id);
-    //    validatedReview.setStatus(ReviewStatus.ACCEPTED);
-    //    return reviewDAO.update(validatedReview);
-    //}
+    @Inject
+    public DraftReviewController(DraftReviewDAO reviewDAO) {
+        this.reviewDAO = reviewDAO;
+    }
+    @GET
+    @Produces(MediaType.APPLICATION_JSON)
+    @AdminGuard
+    public List<DraftReview> getAll() {
+        return reviewDAO.getAll();
+    }
 
-    //@PUT
-    //@Path("/{review_id}/reject")
-    ////@AdminGuard
-    //@Produces(MediaType.APPLICATION_JSON)
-    //public Boolean reject(@PathParam("review_id") int id)
-    //{
-    //    return reviewDAO.delete(id);
-    //}
+    @GET
+    @Path("/{review_id}")
+    @Produces(MediaType.APPLICATION_JSON)
+    @AdminGuard
+    public DraftReview get(@PathParam("review_id") int id) {
+        return reviewDAO.get(id).orElseThrow(() -> new NotFoundException("Review not found"));
+    }
+
+    @POST
+    @Consumes(MediaType.APPLICATION_JSON)
+    @Produces(MediaType.APPLICATION_JSON)
+    @AuthGuard
+    public ApiObjectCreated publish(DraftReview review, @Context SecurityContext sc) {
+        review.setCreatorHash(sc.getUserPrincipal().getName());
+        review.setDate(LocalDate.now());
+
+        int reviewId = reviewDAO.add(review);
+
+        return new ApiObjectCreated(reviewId, "Published review");
+    }
+
+    @DELETE
+    @Path("/{review_id}")
+    @AdminGuard
+    public ApiOk delete(@PathParam("review_id") int id) {
+        this.get(id);
+
+        reviewDAO.delete(id);
+
+        return new ApiOk("Review deleted");
+    }
 }
