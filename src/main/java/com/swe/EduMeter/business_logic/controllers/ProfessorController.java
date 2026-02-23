afc2c00d73b8b8cb8618b9ab5cc9125460b4ae6d
TheRealLorenz <lollobellina@gmail.com>
2026-02-24 00:37:31 +0100

    [Feat] Add ProfessorController

--- src/main/java/com/swe/EduMeter/business_logic/controllers/ProfessorController.java
+++ src/main/java/com/swe/EduMeter/business_logic/controllers/ProfessorController.java
@@ -1,5 +1,80 @@
 package com.swe.EduMeter.business_logic.controllers;
 
+import com.swe.EduMeter.business_logic.auth.annotations.AdminGuard;
+import com.swe.EduMeter.model.Professor;
+import com.swe.EduMeter.model.Teaching;
+import com.swe.EduMeter.model.response.ApiObjectCreated;
+import com.swe.EduMeter.model.response.ApiOk;
+import com.swe.EduMeter.orm.ProfDAO;
+import com.swe.EduMeter.orm.TeachingDAO;
+import jakarta.inject.Inject;
+import jakarta.ws.rs.*;
+import jakarta.ws.rs.core.MediaType;
+
+import java.util.List;
+
+@Path("/prof")
 public class ProfessorController {
-    // TODO search professore filtered by courseId (using Teaching)
+    private final ProfDAO profDAO;
+    private final TeachingDAO teachingDAO;
+
+    @Inject
+    public ProfessorController(ProfDAO profDAO, TeachingDAO teachingDAO) {
+        this.profDAO = profDAO;
+        this.teachingDAO = teachingDAO;
+    }
+
+    @GET
+    @Produces(MediaType.APPLICATION_JSON)
+    public List<Professor> search(@QueryParam("query") String query,
+                                  @QueryParam("course_id") Integer id) {
+        return profDAO.search(query, id);
+    }
+
+    @GET
+    @Path("/{prof_id}")
+    @Produces(MediaType.APPLICATION_JSON)
+    public Professor get(@PathParam("prof_id") int id) {
+        return profDAO.get(id).orElseThrow(() -> new NotFoundException("Professor not found"));
+    }
+
+    @POST
+    @Consumes(MediaType.APPLICATION_JSON)
+    @Produces(MediaType.APPLICATION_JSON)
+    @AdminGuard
+    public ApiObjectCreated create(Professor prof) {
+        return new ApiObjectCreated(profDAO.add(prof), "Professor created");
+    }
+
+    @DELETE
+    @Path("/{prof_id}")
+    @Produces(MediaType.APPLICATION_JSON)
+    @AdminGuard
+    public ApiOk delete(@PathParam("prof_id") int id) {
+        this.get(id);
+        profDAO.delete(id);
+
+        return new ApiOk("Professor deleted");
+    }
+
+    @PUT
+    @Consumes(MediaType.APPLICATION_JSON)
+    @Produces(MediaType.APPLICATION_JSON)
+    public ApiOk update(Professor prof) {
+        if (prof.getId() == null) {
+            throw new BadRequestException("Id must be set");
+        }
+
+        this.get(prof.getId());
+        profDAO.update(prof);
+
+        return new ApiOk("Professor updated");
+    }
+
+    @GET
+    @Path("/{prof_id}/teachings")
+    @Produces
+    public List<Teaching> listTeachings(@PathParam("prof_id") int profId) {
+        return teachingDAO.getByProf(profId);
+    }
 }
